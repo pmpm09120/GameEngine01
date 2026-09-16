9d46bcca8c33d6c02a8959568f138b6d12ab5cbc
gsgsa <seunghyeonan109@gmail.com>
2026-09-16 16:27:21 +0900

    asdf
    sa

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
 
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,13 +11,30 @@ public class Player : MonoBehaviour
     private Vector2 movement;
 
     [Header("PlayerSettings")]
-    public int movespeed = 8;
+    public float movespeed = 8;
+    public float jumpPower = 6;
 
+    [Header("Jump Buffer / GroundTimer")]
+    public float jumpbufferTime = 0.12f;
+    public float jumpbufferCounter;
+    public float groundTime = 0.12f;
+    public float groundTimeCounter;
+
+    [Header("Ground Check")]
+    public bool isGrounded = false;
+    public LayerMask groundlayer;
+    //RayCast
+
+    [Header("Gravity")]
+    public float addGravity = 1.2f;
     public void OnMove(InputAction.CallbackContext callback)
     {
         movement = callback.ReadValue<Vector2>();
     }
-
+    private void Update()
+    {
+        PlayerControl();
+    }
     private void FixedUpdate()
     {
         PlayerMovement();
@@ -26,5 +44,10 @@ public class Player : MonoBehaviour
     {
         Vector2 dir = new Vector2(movement.x * movespeed, rb.linearVelocity.y);
         rb.linearVelocity = dir;
+    }
+
+    void PlayerControl()
+    {
+
     }
 }
